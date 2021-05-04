8a27b22b450bdaaa33f697c8df99e6bcd3384432
danangkonang <dngrifai21@gmail.com>
2021-05-04 21:03:05 +0700

    clear cavhed

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
-        danangkonang github. io
+        danangkonang.github.io
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
@@ -62,17 +43,5 @@ export default {}
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
